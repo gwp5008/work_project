98bb3a607f66d22e5b0848a2df8cb302b54f397b
unknown <gwp5008@yahoo.com>
2022-07-11 14:06:30 -0600

    Got the side panel repo panel to show up correctly.

--- src/components/RepoSidePanelComponent/RepoSidePanelComponent.js
+++ src/components/RepoSidePanelComponent/RepoSidePanelComponent.js
@@ -1,46 +1,38 @@
 import "./RepoSidePanelComponent.css";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import DragAndDropTreeviewComponent from "../DragAndDropTreeviewComponent/DragAndDropTreeviewComponent";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 
 function RepoSidePanelComponent() {
-  const [repositories, setRepositories] = useState([]);
   const [panelItems, setPanelItems] = useState([]);
   const [treeviews, setTreeviews] = useState([]);
 
   useEffect(() => {
-    axios.get("fake repo endpoint").then((response) => {
-      setRepositories(response);
-      console.log("repositories = ");
-      console.log(repositories);
+    const loadData = async () => {
+      const url = "Some dummy URL";
+      const res = await fetch(url).then((res) => res.json());
+
       let tempPanelItems = [];
-      let tempTreeViews = [];
-      for (let i = 0; i < repositories.length; i++) {
-        let tab = <Tab key={repositories[i].id}>{repositories[i].name}</Tab>;
+      let tempTreeviews = [];
+      for (let i = 0; i < res.length; i++) {
+        let tab = <Tab key={res[i].id}>{res[i].name}</Tab>;
         let tabPanel = (
-          <TabPanel key={repositories[i].id}>
+          <TabPanel key={res[i].id}>
             <DragAndDropTreeviewComponent
               index={i}
-              repo={repositories[i].name}
-              key={repositories[i].id}
+              repo={res[i].name}
+              key={res[i].id}
             ></DragAndDropTreeviewComponent>
           </TabPanel>
         );
         tempPanelItems.push(tab);
-        tempTreeViews.push(tabPanel);
+        tempTreeviews.push(tabPanel);
       }
-      console.log("thePanelItems = ");
-      console.log(tempPanelItems);
-      console.log("theTreeViews = ");
-      console.log(tempTreeViews);
       setPanelItems(tempPanelItems);
-      console.log("panelItems = ");
-      console.log(panelItems);
-      setTreeviews(tempTreeViews);
-      console.log("treeviews = ");
-      console.log(treeviews);
-    });
+      setTreeviews(tempTreeviews);
+    };
+
+    loadData();
   }, []);
 
   return (
@@ -49,7 +41,6 @@ function RepoSidePanelComponent() {
         <TabList overflowY="auto" maxHeight="100vh">
           {panelItems}
         </TabList>
-
         <TabPanels>{treeviews}</TabPanels>
       </Tabs>
     </React.Fragment>
